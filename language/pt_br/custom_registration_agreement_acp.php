ab6e70ed43c004ff699dbed72ff0d3db6e74a8d6
eunaumtenhoid <eunaumtenhoid@outlook.com>
2020-05-03 14:08:05 -0300

    ADD Traducao

---
+++ language/pt_br/custom_registration_agreement_acp.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* Custom Registration Agreement extension for the phpBB Forum Software package.
+* Brazilian Portuguese translation by eunaumtenhoid [2020][ver 1.0.0] (https://github.com/phpBBTraducoes)
+* @copyright (c) 2017 Rubén Calvo <rubencm@gmail.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ENABLE_CUSTOM_AGREEMENT'					=> 'Ativar Contrato de registro personalizado',
+	'ENABLE_CUSTOM_AGREEMENT_EXPLAIN'			=> 'Se estiver desativado, a mensagem padrão será exibida.',
+
+	'CUSTOM_AGREEMENT'							=> 'Contrato de registro personalizado',
+	'CUSTOM_AGREEMENT_EXPLAIN'					=> '',
+
+	'CUSTOM_AGREEMENT_PREVIEW'					=> 'Contrato de registro personalizado - Prever',
+));
---
+++ language/pt_br/info_acp_custom_registration_agreement.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* Custom Registration Agreement extension for the phpBB Forum Software package.
+* Brazilian Portuguese translation by eunaumtenhoid [2020][ver 1.0.0] (https://github.com/phpBBTraducoes)
+* @copyright (c) 2017 Rubén Calvo <rubencm@gmail.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	// ACP Module
+	'ACP_CUSTOM_REGISTRATION_AGREEMENT_TITLE'	=> 'Contrato de registro personalizado',
+	'ACP_SETTINGS'								=> 'Configurações',
+));
